06431447d3fbfd001e4e7081d3227e491e43ac83
Jaime Bosch <bombosch7@gmail.com>
2023-09-09 14:20:26 -0600

    Solucionando

--- src/Components/LateralMenu.jsx
+++ src/Components/LateralMenu.jsx
@@ -12,7 +12,7 @@ export const LateralMenu = () => {
   const Descargando = () => {
     // URL del PDF que deseas descargar
     const pdfURL =
-      "https://damarisbarbosamorenodev.azurewebsites.net/cv/Damaris%20CV.pdf";
+      "https://damarisprogramadoraycontadora.com/cv/Damaris%20CV.pdf";
 
     // Crea un enlace temporal para descargar el PDF
     const link = document.createElement("a");
